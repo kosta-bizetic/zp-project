6e0b6206828a4ee1935f751bc5c532f989b934f1
lukad97 <l.dojcilovic@gmail.com>
2020-05-28 23:26:05 +0200

    Added actions menu GUI

--- zp-project/src/bk160121d_dl160135d/Main.java
+++ zp-project/src/bk160121d_dl160135d/Main.java
@@ -1,40 +1,108 @@
 package bk160121d_dl160135d;
 
-import java.io.FileOutputStream;
-import java.security.Security;
-import java.util.LinkedList;
-import java.util.List;
+import java.awt.Frame;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openpgp.PGPEncryptedData;
-import org.bouncycastle.openpgp.PGPPublicKey;
+public class Main extends Frame {
+    private static final long serialVersionUID = 1L;
 
-public class Main {
+    private void createKeysHandler() {
+        System.out.println("create");
+    }
 
-        public static void main(
-            String[] args)
-            throws Exception
-        {
-            Security.addProvider(new BouncyCastleProvider());
+    private void enryptHandler() {
+        System.out.println("encrypt");
+    }
 
-            KeyManagement keyManagement = KeyManagement.getInstance();
+    private void decryptHandler() {
+        System.out.println("decrypt");
+    }
 
-            List<PGPPublicKey> pubKeys = new LinkedList<PGPPublicKey>();
+    private void verifyHandler() {
+        System.out.println("verify");
+    }
 
-            pubKeys.add(keyManagement.getPublicKey(688238868389858045l));
-            pubKeys.add(keyManagement.getPublicKey(6921129671440841737l));
+    private void addMenu() {
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu("Actions");
+        menu.add("Create key pair");
+        menu.add("Encrypt/Sign");
+        menu.add("Decrypt");
+        menu.add("Verify");
+        menu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String action = e.getActionCommand();
+                switch (action) {
+                case "Create key pair":
+                    createKeysHandler();
+                    break;
+                case "Encrypt/Sign":
+                    enryptHandler();
+                    break;
+                case "Decrypt":
+                    decryptHandler();
+                    break;
+                case "Verify":
+                    verifyHandler();
+                    break;
+                }
+            }
+        });
+        menuBar.add(menu);
+        setMenuBar(menuBar);
+    }
 
-            FileOutputStream out = new FileOutputStream("message.txt.sig");
+    private void addComponents() {
+        addMenu();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+                dispose();
+            }});
+    }
 
-            SignatureManagment.signFile("message.txt", 688238868389858045l, out, "stagod".toCharArray());
+    public Main() {
+        super("OpenPGP");
+        setBounds(300, 300, 600, 300);
+        setResizable(false);
+        addComponents();
+        setVisible(true);
+    }
 
-            CryptionManagement.encryptFile("message.txt.sig.gpg", "message.txt.sig", pubKeys , false, true, PGPEncryptedData.IDEA);
+    public static void main(String[] args) {
+        new Main();
+    }
 
-            CryptionManagement.decryptFile("message.txt.sig.gpg", keyManagement.getSecretKeyRingCollection(), "stagod".toCharArray());
-
-            SignatureManagment.verifyFile("message.txt.sig");
-
+//        public static void main(
+//            String[] args)
+//            throws Exception
+//        {
+//            Security.addProvider(new BouncyCastleProvider());
+//
+//            KeyManagement keyManagement = KeyManagement.getInstance();
+//
+//            List<PGPPublicKey> pubKeys = new LinkedList<PGPPublicKey>();
+//
+//            pubKeys.add(keyManagement.getPublicKey(688238868389858045l));
+//            pubKeys.add(keyManagement.getPublicKey(6921129671440841737l));
+//
+//            FileOutputStream out = new FileOutputStream("message.txt.sig");
+//
+//            SignatureManagment.signFile("message.txt", 688238868389858045l, out, "stagod".toCharArray());
+//
+//            CryptionManagement.encryptFile("message.txt.sig.gpg", "message.txt.sig", pubKeys , false, true, PGPEncryptedData.IDEA);
+//
+//            CryptionManagement.decryptFile("message.txt.sig.gpg", keyManagement.getSecretKeyRingCollection(), "stagod".toCharArray());
+//
+//            SignatureManagment.verifyFile("message.txt.sig");
+//
 //            keyManagement.printSecretKeyRingCollection();
-            keyManagement.close();
-        }
+//            keyManagement.close();
+//        }
 }
